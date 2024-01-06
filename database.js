361042541b840beb5fbf684ae7fff675c7c9ff39
jh773 <jinhyeok773@gmail.com>
2024-01-06 19:17:39 +0900

    1

--- database.js
+++ database.js
@@ -3,33 +3,34 @@ import { world } from "@minecraft/server";
 export class database {
     constructor(name){
         this.name = `database:${name}`;
-        world.setDynamicProperty(this.name,JSON.stringify({}));
+        if(world.getDynamicProperty(this.name) == undefined) world.setDynamicProperty(this.name,JSON.stringify({}));
     }
     set(key,value){
         let prev = JSON.parse(world.getDynamicProperty(this.name));
-        prev[key] = value;
+        prev[JSON.stringify(key)] = value;
         world.setDynamicProperty(this.name,JSON.stringify(prev));
     }
     get(key){
-        return JSON.parse(world.getDynamicProperty(this.name))[key]?.value;
+        return JSON.parse(world.getDynamicProperty(this.name))[JSON.stringify(key)];
     }
     keys(){
-        return Object.keys(JSON.parse(world.getDynamicProperty(this.name)));
+        return Object.keys(JSON.parse(world.getDynamicProperty(this.name))).map(x=>JSON.parse(x));
     }
     values(){
         return Object.values(JSON.parse(world.getDynamicProperty(this.name)));
     }
     entries(){
-        return Object.entries(JSON.parse(world.getDynamicProperty(this.name)));
+        return Object.entries(JSON.parse(world.getDynamicProperty(this.name))).map(x=> x[0] = JSON.parse(x[0]));
     }
     has(key){
-        return key in world.getDynamicProperty(this.name);
+        return JSON.parse(world.getDynamicProperty(this.name)).hasOwnProperty(JSON.stringify(key))
     }
     clear(){
-        return world.setDynamicProperty(this.name,undefined);
+        return world.setDynamicProperty(this.name,JSON.stringify({}));
     }
     delete(key){
         let d = JSON.parse(world.getDynamicProperty(this.name));
-        return world.setDynamicProperty(this.name,JSON.stringify(d.filter(x=> x.key != key)));
+        d[JSON.stringify(key)] = undefined;
+        return world.setDynamicProperty(this.name,JSON.stringify(d));
     }
 }
